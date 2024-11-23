6ccee736fc8c7d946127c9bb287e61764c879f52
WedithaV <wedithav@gmail.com>
2024-11-23 20:20:47 +0530

    Change database

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab9b99f406f1cd95cadbd8b517720a6e2a0925e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c8d64fe468f053cd9b0bed80e2a12d58f6eee84")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
