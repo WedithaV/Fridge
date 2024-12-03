d50a3b571cdafa16a787279a2c0d95614dcaac69
WedithaV <wedithav@gmail.com>
2024-12-03 13:54:09 +0530

    Change the widtha of item name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -197,7 +197,7 @@ const App = () => {
           {data && data.length > 0 ? data.map((item, index) => (
             <li key={index} className="flex justify-between items-center bg-white p-4 rounded-md shadow-sm border border-gray-200 hover:outline hover:outline-2 hover:outline-blue-500">
               <div className="flex justify-between items-center w-full">
-                <div className="flex-1">
+                <div className="flex-shrink w-40">
                   <p className="text-gray-800 font-semibold inline mr-10 cursor-pointer" onClick={() => handleEdit(item.id)}>{item.name}</p>
                 </div>
                 <div className="flex-1 text-left">
