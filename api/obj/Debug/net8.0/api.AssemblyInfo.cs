2b8c23983c0a411684f953706c8098f6baf4316d
WedithaV <wedithav@gmail.com>
2024-12-04 11:33:12 +0530

    Center the Loading text

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d50a3b571cdafa16a787279a2c0d95614dcaac69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+249d33b3f55a574dd277c7aedc259078e3f673a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
